7e65bdcf02c0e2ee11a5a27d7b1925ace31aca6f
IlikeChooros <pawel.smolnicki@gmail.com>
2025-10-27 22:26:02 +0100

    removed transform, added minimum y bound and ylim arg for plotting

--- rach-prob/l1/main.py
+++ rach-prob/l1/main.py
@@ -32,6 +32,7 @@ class MonteCarloIntegral:
     N_MAX = 5000
     N_STEP = 50
 
+    m: float
     sup: float
     a: float
     b: float
@@ -40,24 +41,46 @@ class MonteCarloIntegral:
     is_negative: bool
     checkFn: FnCheckType
 
-    def __init__(self, fn: VectorizedFn, a: float, b: float, sup: float | None = None, checkFn: FnCheckType | None = None):
-        self.fn = fn
-        self.fn = fn
+    def __init__(self, fn: VectorizedFn | None, a: float, b: float, m: float = 0.0, sup: float | None = None, checkFn: FnCheckType | None = None):
+        """
+
+        Args:
+            fn (VectorizedFn | None): the main function to integrate, 
+                if specified must be vectorized (accepts both arrays and single values as inputs)
+            a (float): Begining of integration section
+            b (float): End of integration section (must always meet a < b)
+            m (float, optional): The bottom value of the y axis to probe from, defaults to 0
+            sup (float | None, optional): Max/min (min if the function is negative) 
+                value of the function in the [a,b]. Defaults to None.
+            checkFn (FnCheckType | None, optional): Function to check if given point (or points) is under the curve, 
+                fn must NOT be specified. Defaults to None.
+
+        Raises:
+            ValueError: If fn != None and checkFn != None, or both not specified, or the `fn` has
+                alternating values at [a,b].
+        """
+
         self.a = a
         self.b = b
+        self.m = m
+
+        if fn and checkFn:
+            raise ValueError(
+                'Either pass function or checking function, not both.')
 
         # Calculate maximum/minimum value on [a, b] for the provided function
-        if sup == None:
+        if sup == None and fn != None:
             def calc_deriv(x: VectorVType) -> VectorVType:
                 D_EPSILON = 0.0001
-                y = self.fn(x)
-                dy = self.fn(x + D_EPSILON)
+                y = fn(x)
+                dy = fn(x + D_EPSILON)
                 return (dy - y) / D_EPSILON
 
             # Choose the most promising starting point
             # and then use gradient ascent or descent to find the max/min value
             xs = np.linspace(a, b, 20)
             ys = fn(xs)
+            
             self.is_negative = bool(np.all(ys <= 0))
             if np.any(ys < 0) and np.any(ys > 0):
                 raise ValueError(
@@ -65,7 +88,7 @@ class MonteCarloIntegral:
 
             x = 0.0
 
-            # Use gradient ascent to find maximum
+            # Use gradient ascent/descend to find maximum/minimum
             d = 0.0
             EPSILON = 0.01
             ALPHA = 0.15
@@ -91,8 +114,15 @@ class MonteCarloIntegral:
 
             # Evaluate function at the found maximum/minimum point
             sup = float(fn(x))
+            
+        elif sup == None:
+            raise ValueError('Sup must be specified if "fn" is None.')
 
         if checkFn == None:
+            if fn == None:
+                raise ValueError(
+                    'Expected specified check function (main function is not specified)')
+
             # Check if the point(s) is under the curve
             def defaultCheck(f: VectorizedFn, v: FnCheckVArg) -> Union[bool, BoolArray]:
                 xs, ys = v
@@ -104,6 +134,7 @@ class MonteCarloIntegral:
 
             checkFn = defaultCheck
 
+        self.fn = fn if fn != None else lambda x: x
         self.sup = sup
         self.checkFn = checkFn
         self.gen = Generator(MT19937())
@@ -133,37 +164,36 @@ class MonteCarloIntegral:
                 self.checkFn(
                     self.fn, (
                         gen.random((n,)) * (self.b - self.a) + self.a,
-                        gen.random((n,)) * self.sup
+                        gen.random((n,)) * self.sup + self.m
                     )))
-            results[i] = (c/n*(self.b-self.a)*self.sup)
+            results[i] = (c/n*(self.b-self.a)*(self.sup-self.m))
         return results
 
 
 def run_experiment(
-        mci: MonteCarloIntegral, k: int, expected: float, 
-        title: str | None = None, filename: str | None = None, 
-        transform: None | TransformType = None, showplt: bool = True):
+        mci: MonteCarloIntegral, k: int, expected: float,
+        title: str | None = None, filename: str | None = None,
+        ylim: typ.Tuple[float, float] | None = None,
+        showplt: bool = True):
     result = mci.go(k)
     y, x = mci.as_plt(k, result)
 
     meany = np.mean(np.array([arr for arr in result.values()]), axis=1)
     meanx = mci.n_range()
-
-    if transform != None:
-        meany = transform(meany)
-        y = transform(y)
-
     fig, ax = plt.subplots()
 
     ax.scatter(x, y, s=5, color="#38B0E8")
     ax.scatter(meanx, meany, s=20, color="#BA23D3")
     ax.plot([meanx[0], meanx[len(meanx)-1]],
-             [expected, expected], color="#D3BA23", linewidth=2)
+            [expected, expected], color="#D3BA23", linewidth=2)
 
     if title:
         fig.suptitle(f'{title} k={k}')
     ax.xaxis.set_label('N')
     ax.yaxis.set_label('Approx Value')
+
+    if ylim:
+        ax.set_ylim(ylim)
     ax.grid(True)
 
     if filename != None:
@@ -186,56 +216,45 @@ def get_args() -> arg.Namespace:
         '-f', '--func', default=FUNCS[0], choices=FUNCS, type=str, help='Function to integrate')
     p.add_argument('-s', '--filename', default=None, type=str,
                    help='Filename for the saved graph')
-    p.add_argument('-t', '--title', default='Monte Carlo Integration', type=str, help='Set the title of the plot')
-    
-    p.add_argument('-r', '--run_all', action='store_true', default=False, 
-                    help='Run all experiments metioned in the exercise (poly, sin, cbrt, pi) with k=[5, 50]')
+    p.add_argument('-t', '--title', default='Monte Carlo Integration',
+                   type=str, help='Set the title of the plot')
+
+    p.add_argument('-r', '--run_all', action='store_true', default=False,
+                   help='Run all experiments metioned in the exercise (poly, sin, cbrt, pi) with k=[5, 50]')
     return p.parse_args()
 
 
 def main():
     args = get_args()
 
-    # Create vectorized versions of the integration functions
-    def vectorized_poly(x: VectorVType) -> VectorVType:
-        return 4*x*(x-1)**3
-
-    # No actual function for the half of circle, because
-    # I'm using the `is_in_cricle` custom hook
-    # for checking if the point is inside the circle
-
-    def dummy_circle(x: VectorVType) -> VectorVType:
-        return 0.0
-
     # Simply check if given 2 points are inside the unit circle
-    def is_in_circle(f: VectorizedFn, v: FnCheckVArg) -> Union[bool, BoolArray]:
-        x, y = v
-        return (x ** 2 + y**2) <= 1
-
-    def transform_circle(p: FloatArray) -> FloatArray:
-        return p * 2
+    is_in_circle: FnCheckType = lambda f, v: (v[0]**2 + v[1]**2) <= 1
 
     # MonteCarloIntegral: function, a, b, [checkFn]
     integrals = (
-        (MonteCarloIntegral(lambda x: x**3, 1, 3), 20.0, None, 'x^3'),
-        (MonteCarloIntegral(lambda x: np.cbrt(x), 0, 8), 12.0, None, 'Cbrt'),
-        (MonteCarloIntegral(lambda x: np.sin(x), 0, np.pi), 2.0, None, 'Sin'),
-        (MonteCarloIntegral(vectorized_poly, 0, 1), -0.2, None, 'Poly'),
-        (MonteCarloIntegral(dummy_circle, -1, 1, 1, is_in_circle), np.pi, transform_circle, 'Pi')
+        (MonteCarloIntegral(lambda x: x**3, 1, 3), 20.0, 'x^3', (20.0 - 2, 20.0 + 2)),
+        (MonteCarloIntegral(lambda x: np.cbrt(x), 0, 8), 12.0, 'Cbrt', (12.0 - 2, 12.0 + 2)),
+        (MonteCarloIntegral(lambda x: np.sin(x), 0, np.pi), 2.0, 'Sin', (2.0 - 0.3, 2.0 + 0.3)),
+        (MonteCarloIntegral(lambda x: 4*x*(x-1)**3, 0, 1), -0.2, 'Poly', (-0.20 - 0.04, -0.20 + 0.04)),
+        (MonteCarloIntegral(None, -1, 1, m=-1, sup=1, checkFn=is_in_circle),
+         np.pi, 'Pi', (np.pi - 0.3, np.pi + 0.3))
     )
 
     print(args._get_kwargs())
 
     if not args.run_all:
-        mci, expected, transform, _ = integrals[FUNCS.index(args.func)]
-        run_experiment(mci, args.k, expected, args.title, args.filename, transform)
+        mci, expected, _, ylim = integrals[FUNCS.index(args.func)]
+        run_experiment(mci, args.k, expected, args.title,
+                       args.filename, ylim=ylim)
     else:
         KS = [5, 50]
         print('Running all experiments with k={}'.format(KS))
 
         for k in KS:
-            for mci, expected, transform, title in integrals:
-                run_experiment(mci, k, expected, None, f'{title}-{k}.png', transform, showplt=False)
+            for mci, expected, title, ylim in integrals:
+                run_experiment(
+                    mci, k, expected, None, f'{title}-{k}.png', ylim=ylim, showplt=False)
+
 
 if __name__ == '__main__':
     main()
